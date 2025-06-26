aab81d2cac7fbff160d00e5c6e5d6cc42dbba482
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-06-27 01:32:27 +0300

    Add base colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,33 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './app/**/*.{ts,tsx}',        
-    './components/**/*.{ts,tsx}', 
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
   ],
+  darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          light: '#FFFFFF',
+          dark: '#090D1F',
+        },
+        text: {
+          primary: {
+            light: '#1A1A1A',
+            dark: '#FFFFFF',
+          },
+          secondary: {
+            light: '#667085',
+            dark: '#C0C5D0',
+          },
+          title: {
+            light: '#6941C6',
+            dark: '#6941C6',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
